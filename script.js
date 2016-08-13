c3007f8c199591cc9d26a6e75f6415253d6eb1d3
Sebastien Chatelle Blanchard <sebastien.chatelle.blanchard@gmail.com>
2016-08-13 16:46:43 -0400

    update 13-08-2016 #2

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-
-
+	for (var i = 0; i < 256; i++) {
+		if (i%16==0 && i != 0) {
+			$('<br></br>').appendTo(playarea);			
+			$('<div></div>', {
+			"class": 'square',
+			}).appendTo(playarea);
+		}else{
+			$('<div></div>', {
+			"class": 'square',
+			}).appendTo(playarea);
+		}
+	};
+	$('.square').on('mouseenter', function() {
+		$(this).css('background-color', 'white');
+	})
+	$('#clear').click(function(){
+		$('.square').css('background-color', 'black');
+	})
 });
